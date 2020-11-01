e6c3356fa4480752575a3fdfe9c57ef80316c8f8
stefnotch <brandmairstefan@gmail.com>
2020-11-01 14:10:18 +0100

    Add KaTeX

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { createRouter, createWebHashHistory } from "vue-router";
 import App from "./App.vue";
 import "./index.css";
 import Overview from "./pages/Overview.vue";
+//import { render } from "katex/dist/katex.mjs";
 
 /** @type {import("vue-router").RouteRecordRaw[]} */
 const routes = [{ path: "/", component: Overview }];
