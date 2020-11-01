4a308181bc709c6c09699e09038b229a7c1d6e5d
stefnotch <brandmairstefan@gmail.com>
2020-11-01 21:49:01 +0100

    Spent way too much time on the whole error message thing

--- src/MathJson.ts
+++ src/MathJson.ts
@@ -1,1 +1,15 @@
-export type MathJson = [];
+export type MathJsonData = boolean;
+
+export type MathJsonOperator =
+  | "not"
+  | "implies"
+  | "and"
+  | "or"
+  | "xor"
+  | "nand"
+  | "nor"
+  | "equals";
+
+export type MathJson =
+  | [operator: MathJsonOperator, ...args: MathJson[]]
+  | MathJsonData;
--- src/components/MathInput.vue
+++ src/components/MathInput.vue
@@ -2,6 +2,7 @@
   <div class="math-input">
     <input type="text" v-model="mathinput" size="100" />
     <div ref="mathoutput"></div>
+    <div class="error"></div>
   </div>
 </template>
 
@@ -20,10 +21,32 @@ import Katex from "katex";
 import { parse as parseAstLogical } from "./../assets/grammar-logical";
 
 function useLogicalParsing() {
-  function parseLogical(value: string): MathJson {
-    console.log(parseAstLogical(value));
-    //@ts-ignore
-    return [value];
+  function parseLogical(
+    value: string
+  ): { mathJson?: MathJson; error?: string } {
+    // Doesn't have a AND before OR order of operations (yet)
+    const parsed = parseAstLogical(value.replace(/[ \n\t]*/g, ""));
+    if (parsed.err) {
+      return { error: "" + parsed.err };
+    }
+
+    function toMathJson(ast: any) {
+      if (ast.left) {
+        return [
+          ast.operator.value,
+          toMathJson(ast.left),
+          toMathJson(ast.right),
+        ];
+      } else if (ast.right && ast.operator) {
+        return [ast.operator.value, toMathJson(ast.right)];
+      } else if (ast.right) {
+        return toMathJson(ast.right);
+      } else {
+        return ast.value;
+      }
+    }
+
+    return { mathJson: toMathJson(parsed.ast) };
   }
 
   return {
@@ -32,7 +55,7 @@ function useLogicalParsing() {
 }
 
 function useMathParsing() {
-  function parseMath(value: string): MathJson {
+  function parseMath(value: string): { mathJson?: MathJson; error?: string } {
     console.log(parseAstLogical(value));
     //@ts-ignore
     return [value];
@@ -44,9 +67,20 @@ function useMathParsing() {
 }
 
 function useMathPrinting() {
-  function mathToLatex(value: MathJson) {
-    //@ts-ignore
-    return value[0];
+  function mathToLatex(value: { mathJson?: MathJson; error?: string }): string {
+    if (value.error) {
+      // https://tex.stackexchange.com/a/34586
+      let escaped = value.error
+        .replace(/\\/g, "\\textbackslash")
+        .replace(/[&%$#_{}]/g, "\\$&")
+        .replace(/\[/g, "{[}")
+        .replace(/\]/g, "{]}")
+        .replace(/~/g, "{\\textasciitilde}")
+        .replace(/\^/g, "{\\textasciicircum}");
+      return `\\textcolor{red}{\\texttt{${escaped}}}`;
+    }
+
+    return "";
   }
 
   return {
@@ -68,19 +102,18 @@ export default defineComponent({
     const mathPrinting = useMathPrinting();
 
     const mathinput = ref("");
-    const parsed = ref<any>();
     const mathoutput = ref<HTMLElement>();
 
     onMounted(() => {
       watch(mathinput, (value) => {
         nextTick(() => {
-          if (props.type === "logical") {
-            parsed.value = logicalParsing.parseLogical(value);
-          } else {
-            parsed.value = mathParsing.parseMath(value);
-          }
+          let parsedAst =
+            props.type === "logical"
+              ? logicalParsing.parseLogical(value)
+              : mathParsing.parseMath(value);
 
-          const latex = mathPrinting.mathToLatex(parsed.value);
+          console.log(parsedAst);
+          const latex = mathPrinting.mathToLatex(parsedAst);
 
           Katex.render(latex, mathoutput.value, {
             displayMode: true,
@@ -105,5 +138,8 @@ export default defineComponent({
 }
 .math-input input {
   font-family: "Consolas", "Courier New", Courier, monospace;
+}
+.error {
+  color: red;
 }
 </style>
