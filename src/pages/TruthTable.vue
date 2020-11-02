9d9777893fa59dada7597b9c18a9026502783c3e
stefnotch <brandmairstefan@gmail.com>
2020-11-02 16:31:46 +0100

    Implement truth table evaluation

--- src/pages/TruthTable.vue
+++ src/pages/TruthTable.vue
@@ -7,7 +7,7 @@
     @mathJson="(value) => (logicalMathJson = value)"
   ></math-input>
 
-  <table>
+  <table class="truth-table">
     <thead>
       <tr>
         <th v-for="(item, index) in tableHeaders" :key="index">
@@ -15,9 +15,13 @@
         </th>
       </tr>
     </thead>
-    <tbody class="monospace">
+    <tbody>
       <tr v-for="(row, index) in tableRows" :key="index">
-        <td v-for="(item, itemIndex) in row" :key="itemIndex">
+        <td
+          v-for="(item, itemIndex) in row"
+          :key="itemIndex"
+          :class="{ 'faded-text': item === false }"
+        >
           {{ item === true ? 1 : item === false ? 0 : item }}
         </td>
       </tr>
@@ -36,7 +40,7 @@ import {
 } from "vue";
 import { useRoute, useRouter } from "vue-router";
 import { BinaryNumber } from "../math/binary-number";
-import { MathJson } from "../MathJson";
+import { MathJson, MathJsonLogicalOperator } from "../MathJson";
 import MathInput from "./../components/MathInput.vue";
 import MathOutput from "./../components/MathOutput.vue";
 
@@ -101,9 +105,59 @@ function useLogicalMath() {
     return operations;
   }
 
+  const mathJsonOperatorMap = new Map<
+    MathJsonLogicalOperator,
+    (a: boolean, b?: boolean) => boolean
+  >([
+    ["not", (a) => !a],
+    ["implies", (a, b) => !a || b],
+    ["and", (a, b) => a && b],
+    ["or", (a, b) => a || b],
+    ["xor", (a, b) => (a ? !b : b)],
+    ["nand", (a, b) => !(a && b)],
+    ["nor", (a, b) => !(a || b)],
+    ["equals", (a, b) => a == b],
+  ]);
+
+  function evaluateRecursive(
+    ast: MathJson,
+    getters: Map<string, boolean>
+  ): boolean {
+    if (Array.isArray(ast)) {
+      const op = mathJsonOperatorMap.get(ast[0]);
+      if (!op) throw new Error("Unknown operation " + ast);
+
+      if (ast.length === 3) {
+        return op(
+          evaluateRecursive(ast[1], getters),
+          evaluateRecursive(ast[2], getters)
+        );
+      } else if (ast.length === 2) {
+        return op(evaluateRecursive(ast[1], getters));
+      } else {
+        throw new Error("Unable to evaluate " + ast);
+      }
+    } else if (ast === true) {
+      return true;
+    } else if (ast === false) {
+      return false;
+    } else if (typeof ast === "string") {
+      const result = getters.get(ast);
+      if (result === undefined) throw new Error("Unable to evaluate " + ast);
+      return result;
+    } else {
+      throw new Error("Unable to evaluate " + ast);
+    }
+  }
+
+  function evaluate(ast: MathJson, getters?: Map<string, boolean>): boolean {
+    return evaluateRecursive(ast, getters ?? new Map());
+  }
+
   return {
     extractGetters,
     extractOperations,
+    evaluate,
   };
 }
 
@@ -122,29 +176,34 @@ export default defineComponent({
 
     watch(logicalMathJson, (value) => {
       console.log(value);
-      const getters = logicalMath.extractGetters(value);
+      const getterNames = logicalMath.extractGetters(value);
       let operations = logicalMath.extractOperations(value);
 
-      tableHeaders.value = (Array.from(getters) as MathJson[]).concat(
-        operations
-      );
+      const getters = Array.from(logicalMath.extractGetters(value));
 
-      let binaryNumber = BinaryNumber.fromSize(getters.size);
-      const oneInBinary = BinaryNumber.fromSize(getters.size).add(
+      tableHeaders.value = (getters as MathJson[]).concat(operations);
+
+      let binaryNumber = BinaryNumber.fromSize(getters.length);
+      const oneInBinary = BinaryNumber.fromSize(getters.length).add(
         new BinaryNumber([true])
       );
 
       const tableWidth = tableHeaders.value.length;
-      const tableData = new Array(2 ** getters.size);
+      const tableData = new Array(2 ** getters.length);
       for (let i = 0; i < tableData.length; i++) {
         tableData[i] = new Array(tableWidth);
 
-        for (let j = 0; j < getters.size; j++) {
+        for (let j = 0; j < getters.length; j++) {
           tableData[i][j] = binaryNumber.value[j];
         }
+        const getterData = new Map<string, boolean>(
+          getters.map((v, j) => [v, binaryNumber.value[j]])
+        );
 
-        for (let j = getters.size; j < tableWidth; j++) {
-          //tableData[i][j] =
+        for (let j = getters.length; j < tableWidth; j++) {
+          const op = operations[j - getters.length];
+
+          tableData[i][j] = logicalMath.evaluate(op, getterData);
         }
 
         binaryNumber = binaryNumber.add(oneInBinary);
@@ -168,7 +227,32 @@ export default defineComponent({
 </script>
 
 <style scoped>
-.monospace {
+.truth-table {
   font-family: "Consolas", "Courier New", Courier, monospace;
+  text-align: center;
+  overflow: hidden;
+}
+.truth-table td {
+  padding: 2px;
+}
+.truth-table th {
+  position: relative;
+}
+.faded-text {
+  color: #7c7c7c;
+}
+.truth-table tbody tr:hover {
+  background-color: #f1f1f1;
+}
+
+.truth-table th:hover::after {
+  background-color: #f1f1f1;
+  content: "";
+  position: absolute;
+  left: 0;
+  top: -5000px;
+  height: 10000px;
+  width: 100%;
+  z-index: -1;
 }
 </style>
