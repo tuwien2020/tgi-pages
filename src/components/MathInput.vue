871d7ee2818900a620cfa7e993828aa987b25bac
stefnotch <brandmairstefan@gmail.com>
2020-11-02 10:04:12 +0100

    Replaced parser library

--- src/components/MathInput.vue
+++ src/components/MathInput.vue
@@ -16,37 +16,38 @@ import {
   onMounted,
   nextTick,
 } from "vue";
-import { MathJson } from "./../MathJson";
+import { MathJson, MathJsonLogicalOperator } from "./../MathJson";
 import Katex from "katex";
-import { parse as parseAstLogical } from "./../assets/grammar-logical";
+import { tryParse as tryParseAstLogical } from "./../assets/grammar-logical";
 
 function useLogicalParsing() {
+  function toMathJsonRecursive(ast: any) {
+    if (ast.left) {
+      return [
+        ast.operator,
+        toMathJsonRecursive(ast.left),
+        toMathJsonRecursive(ast.right),
+      ];
+    } else if (ast.right && ast.operator) {
+      return [ast.operator, toMathJsonRecursive(ast.right)];
+    } else if (ast.right) {
+      return toMathJsonRecursive(ast.right);
+    } else {
+      return ast.value;
+    }
+  }
+
   function parseLogical(
     value: string
   ): { mathJson?: MathJson; error?: string } {
     // Doesn't have a AND before OR order of operations (yet)
-    const parsed = parseAstLogical(value.replace(/[ \n\t]*/g, ""));
-    if (parsed.err) {
-      return { error: "" + parsed.err };
+    try {
+      const parsed = tryParseAstLogical(value);
+      console.log(parsed);
+      return { mathJson: toMathJsonRecursive(parsed) };
+    } catch (e) {
+      return { error: "" + e };
     }
-
-    function toMathJson(ast: any) {
-      if (ast.left) {
-        return [
-          ast.operator.value,
-          toMathJson(ast.left),
-          toMathJson(ast.right),
-        ];
-      } else if (ast.right && ast.operator) {
-        return [ast.operator.value, toMathJson(ast.right)];
-      } else if (ast.right) {
-        return toMathJson(ast.right);
-      } else {
-        return ast.value;
-      }
-    }
-
-    return { mathJson: toMathJson(parsed.ast) };
   }
 
   return {
@@ -56,7 +57,7 @@ function useLogicalParsing() {
 
 function useMathParsing() {
   function parseMath(value: string): { mathJson?: MathJson; error?: string } {
-    console.log(parseAstLogical(value));
+    console.log(tryParseAstLogical(value));
     //@ts-ignore
     return [value];
   }
@@ -67,7 +68,72 @@ function useMathParsing() {
 }
 
 function useMathPrinting() {
+  // TODO: handle (0∨ not 1 ∨ not 0)
+  const mathJsonOperatorMap = new Map<MathJsonLogicalOperator, string>([
+    ["not", "\\neg"],
+    ["implies", "\\implies"],
+    ["and", "\\land"],
+    ["or", "\\lor"],
+    ["xor", "\\mathtt{XOR}"],
+    ["nand", "\\mathtt{NAND}"],
+    ["nor", "\\mathtt{NOR}"],
+    ["equals", "\\Leftrightarrow"],
+  ]);
+
   function mathToLatex(value: { mathJson?: MathJson; error?: string }): string {
+    if (value.mathJson) {
+      function needsBrackets(ast: MathJson) {
+        // For a correct implementation, you'd need to implement operator precedence...
+        if (Array.isArray(ast) && ast.length == 3) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+
+      function toLatexRecursive(ast: MathJson) {
+        if (Array.isArray(ast)) {
+          const op = mathJsonOperatorMap.get(ast[0]);
+          if (!op) {
+            throw new Error("Unknown operator " + ast);
+          }
+
+          if (ast.length == 0) {
+            throw new Error("Not well formed AST tree " + ast);
+          } else if (ast.length == 2) {
+            let right = toLatexRecursive(ast[1]);
+            if (needsBrackets(ast[1])) {
+              right = `(${right})`;
+            }
+            return `${op} ${right}`;
+          } else if (ast.length == 3) {
+            let left = toLatexRecursive(ast[1]);
+            if (needsBrackets(ast[1])) {
+              left = `(${left})`;
+            }
+            let right = toLatexRecursive(ast[2]);
+            if (needsBrackets(ast[2])) {
+              right = `(${right})`;
+            }
+            return `${left} ${op} ${right}`; // TODO:
+          } else {
+            throw new Error("Not well formed AST tree " + ast);
+          }
+        } else if (ast === true) {
+          return "\\mathtt{1}";
+        } else if (ast === false) {
+          return "\\mathtt{0}";
+        }
+      }
+
+      try {
+        const output = toLatexRecursive(value.mathJson);
+        return output;
+      } catch (e) {
+        value.error = e;
+      }
+    }
+
     if (value.error) {
       // https://tex.stackexchange.com/a/34586
       let escaped = value.error
