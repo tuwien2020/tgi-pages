3a6eda02518572e5aac1b154564446f8822cd570
stefnotch <brandmairstefan@gmail.com>
2020-12-06 13:52:51 +0100

    Minor refactor

--- src/components/MathSingleOp.vue
+++ src/components/MathSingleOp.vue
@@ -28,6 +28,256 @@ function useMathWithStepsPrinting() {
   function pad(value: number) {
     return `\\phantom{\\mathtt{${"0".repeat(value)}}}`;
   }
+
+  function calculationToLatex(
+    valueA: BinaryNumber,
+    valueB: BinaryNumber,
+    operator: MathJsonMathOperator,
+    options?: {
+      placesAfterDecimal?: number;
+    }
+  ) {
+    const placesAfterDecimal = options?.placesAfterDecimal ?? 0;
+
+    if (operator == "add" || operator == "subtract") {
+      const result =
+        operator == "add" ? valueA.add(valueB) : valueA.subtract(valueB);
+
+      let output = "";
+
+      const formatLatexNumber = (op: string, value: BinaryNumber) =>
+        `& \\texttt{${op}} & \\mathtt{${bitArray(
+          value.getValueBeforeDecimal()
+        )}} & \\mathtt{${
+          value.decimalPoint > 0
+            ? "." + bitArray(value.getValueAfterDecimal())
+            : ""
+        }} &`;
+
+      // see binary-number.ts
+      if (operator == "subtract") {
+        valueB = valueB.setSign(!valueB.isNegative);
+      }
+
+      if (valueA.isNegative == valueB.isNegative) {
+        // Addition
+        let firstNumber = formatLatexNumber("", valueA);
+        if (result.isNegative) {
+          // Slap a minus sign around the operation
+          firstNumber =
+            `\\smash{\\raisebox{-0.75em}{$-\\Bigg($}}` +
+            firstNumber +
+            `\\smash{\\raisebox{-0.75em}{$\\Bigg)$}}`;
+        }
+        output += firstNumber;
+        output += "\\\\\n";
+        let secondNumber = formatLatexNumber("+", valueB);
+        output += secondNumber;
+      } else {
+        // Subtraction
+        const comparison = valueA
+          .setSign(false)
+          .compareTo(valueB.setSign(false));
+        if (comparison < 0) {
+          const temp = valueA;
+          valueA = valueB;
+          valueB = temp;
+        }
+        let firstNumber = formatLatexNumber("", valueA);
+        if (result.isNegative) {
+          // Slap a minus sign around the operation
+          firstNumber =
+            `\\smash{\\raisebox{-0.75em}{$-\\Bigg($}}` +
+            firstNumber +
+            `\\smash{\\raisebox{-0.75em}{$\\Bigg)$}}`;
+        }
+        output += firstNumber;
+        output += "\\\\\n";
+        let secondNumber = formatLatexNumber("-", valueB);
+        output += secondNumber;
+      }
+
+      output += `\\\\\n\\hline ${formatLatexNumber(
+        result.isNegative ? "-" : "",
+        result
+      )}`;
+
+      output = `\\begin{alignedat}{3}\n${output}\n\\end{alignedat}`;
+      return output;
+    } else if (operator == "multiply") {
+      const result = valueA.multiply(valueB);
+      let output = "";
+
+      const lastLineLength = valueA.value.length + valueB.value.length - 1;
+      const resultLength = result.value.length ?? 0;
+
+      const requiredPadding =
+        resultLength > lastLineLength ? resultLength - lastLineLength : 0;
+
+      output += `& ${pad(requiredPadding)} ${mathPrinting.binaryNumberToLatex(
+        valueA,
+        {
+          zeroWidthDecimal: true,
+          groupZeros: false,
+        }
+      )} \\times ${mathPrinting.binaryNumberToLatex(valueB, {
+        zeroWidthDecimal: true,
+        groupZeros: false,
+      })} \\\\\n`;
+
+      output += `\\hline \\\\\n`;
+
+      // Additions
+      let extraZeros = 0;
+      for (let i = 0; i < valueB.value.length; i++) {
+        const bitB = valueB.value[i];
+        if (bitB) {
+          output += `&${pad(
+            i - extraZeros + requiredPadding
+          )} \\mathtt{${"0".repeat(extraZeros)} ${bitArray(
+            valueA.value
+          )}} \\\\\n`;
+          extraZeros = 0;
+        } else {
+          extraZeros++;
+        }
+      }
+
+      // If the last line is "missing", add it
+      if (extraZeros) {
+        extraZeros--;
+        output += `&${pad(
+          valueB.value.length - 1 - extraZeros + requiredPadding
+        )} \\mathtt{${"0".repeat(extraZeros + valueA.value.length)}} \\\\\n`;
+      }
+
+      output += `\\hline \\\\\n`;
+
+      // Result
+      output += `&${pad(
+        lastLineLength - resultLength + requiredPadding
+      )} ${mathPrinting.binaryNumberToLatex(result, {
+        zeroWidthDecimal: true,
+        groupZeros: false,
+      })}`;
+      output = `\\def\\arraystretch{0.1}\n\\begin{alignedat}{1}\n${output}\n\\end{alignedat}`;
+      return output;
+    } else if (operator == "divide") {
+      // TODO: Catch division by zero
+      const result = valueA.divide(valueB, placesAfterDecimal);
+      let output = "";
+
+      output += `&${pad(1)} ${mathPrinting.binaryNumberToLatex(valueA, {
+        zeroWidthDecimal: true,
+        groupZeros: false,
+      })} ${pad(2)} \\mathllap{\\div} ${pad(
+        1
+      )} ${mathPrinting.binaryNumberToLatex(valueB, {
+        zeroWidthDecimal: true,
+        groupZeros: false,
+      })} ${pad(2)} \\mathllap{=} ${pad(1)}`;
+
+      // Binary division
+      // Shift the decimal points away
+      const maxDecimal = Math.max(valueA.decimalPoint, valueB.decimalPoint);
+      const a = valueA.setSign(false).multiplyByPowerOfTwo(maxDecimal);
+      const b = valueB
+        .setSign(false)
+        .multiplyByPowerOfTwo(maxDecimal)
+        .trimZerosBeforeDecimal();
+
+      if (b.value.length <= 0) {
+        output += `\\text{Division by zero}`;
+      } else {
+        output += ` ${mathPrinting.binaryNumberToLatex(result.result, {
+          zeroWidthDecimal: true,
+          groupZeros: false,
+        })}\\\\\n`;
+
+        // Copy of binary-number.ts
+        let index = 0;
+        const maxLength = a.value.length + placesAfterDecimal; // TODO: Configureable
+        const resultBits = new Array<boolean>(maxLength).fill(false);
+        let remainder = new BinaryNumber(false, [], 0);
+
+        const printPadded = (
+          value: string,
+          valueLength: number,
+          length: number
+        ) =>
+          `&${pad(
+            Math.max(length - valueLength, 0)
+          )} \\mathtt{${value}} \\\\\n`;
+
+        while (!a.isZero() && index < maxLength) {
+          // Pull down the next bit
+          const newBit = index < a.value.length ? a.value[index] : false;
+          remainder = new BinaryNumber(
+            false,
+            remainder.value.concat(newBit),
+            0
+          );
+
+          const remainderToPrint = remainder.trimZerosBeforeDecimal().extend(1);
+
+          output += printPadded(
+            bitArray(remainderToPrint.value),
+            remainderToPrint.value.length,
+            1 + remainder.value.length
+          );
+
+          // If we can subtract, do so
+          if (remainder.compareTo(b) >= 0) {
+            remainder = remainder.subtract(b);
+            resultBits[index] = true;
+
+            const paddingDelta = remainderToPrint.value.length - b.value.length;
+
+            output += printPadded(
+              `\\mathllap{-}\\underline{${pad(paddingDelta)}${bitArray(
+                b.value
+              )}}`,
+              remainderToPrint.value.length,
+              1 + remainder.value.length
+            );
+          } else {
+            resultBits[index] = false;
+            output += printPadded(
+              `\\mathllap{-}\\underline{${pad(
+                remainderToPrint.value.length - 1
+              )}0}`,
+              remainderToPrint.value.length,
+              1 + remainder.value.length
+            );
+          }
+          index += 1;
+        }
+
+        let remainderToPrint = new BinaryNumber(
+          false,
+          result.remainder ?? [],
+          0
+        )
+          .trimZerosBeforeDecimal()
+          .extend(1);
+
+        output += printPadded(
+          bitArray(remainderToPrint.value) + `\\mathrlap{R}`,
+          remainderToPrint.value.length,
+          1 + remainder.value.length
+        );
+      }
+
+      output = `\\def\\arraystretch{0.1}\n\\begin{alignedat}{2}\n${output}\n\\end{alignedat}`;
+      return output;
+    }
+
+    return "";
+  }
+
+  return {
+    calculationToLatex,
+  };
 }
 
 export default defineComponent({
@@ -49,284 +299,24 @@ export default defineComponent({
   },
   setup(props, context) {
     const mathoutput = ref<HTMLElement>();
-    const placesAfterDecimal = 7;
-    const mathPrinting = useMathPrinting();
+    const placesAfterDecimal = ref(7);
+    const mathPrinting = useMathWithStepsPrinting();
 
     function printBitArray(value: readonly boolean[] | undefined) {
       return (value ?? []).map((v) => (v ? "1" : "0")).join("");
     }
 
-    const output = computed(() => {
-      const phantomPadding = (value: number) =>
-        `\\phantom{\\mathtt{${"0".repeat(value)}}}`;
-
-      if (props.operator == "add" || props.operator == "subtract") {
-        const result =
-          props.operator == "add"
-            ? props.valueA.add(props.valueB)
-            : props.valueA.subtract(props.valueB);
-
-        let valueA = props.valueA;
-        let valueB = props.valueB;
-        let output = "";
-
-        const formatLatexNumber = (
-          op: string,
-          beforeDecimal: boolean[],
-          afterDecimal: boolean[]
-        ) =>
-          `& \\texttt{${op}} & \\mathtt{${printBitArray(
-            beforeDecimal
-          )}} & \\mathtt{${
-            afterDecimal.length > 0 ? "." + printBitArray(afterDecimal) : ""
-          }} &`;
-
-        // see binary-number.ts
-        if (props.operator == "subtract") {
-          valueB = valueB.setSign(!valueB.isNegative);
+    const output = computed(() =>
+      mathPrinting.calculationToLatex(
+        props.valueA,
+        props.valueB,
+        props.operator,
+        {
+          placesAfterDecimal: placesAfterDecimal.value,
         }
+      )
+    );
 
-        if (valueA.isNegative == valueB.isNegative) {
-          // Addition
-          let firstNumber = formatLatexNumber(
-            "",
-            valueA.getValueBeforeDecimal(),
-            valueA.getValueAfterDecimal()
-          );
-          if (result.isNegative) {
-            // Slap a minus sign around the operation
-            firstNumber =
-              `\\smash{\\raisebox{-0.75em}{$-\\Bigg($}}` +
-              firstNumber +
-              `\\smash{\\raisebox{-0.75em}{$\\Bigg)$}}`;
-          }
-          output += firstNumber;
-          output += "\\\\\n";
-          let secondNumber = formatLatexNumber(
-            "+",
-            valueB.getValueBeforeDecimal(),
-            valueB.getValueAfterDecimal()
-          );
-          output += secondNumber;
-        } else {
-          // Subtraction
-          const comparison = valueA
-            .setSign(false)
-            .compareTo(valueB.setSign(false));
-          if (comparison < 0) {
-            const temp = valueA;
-            valueA = valueB;
-            valueB = temp;
-          }
-          let firstNumber = formatLatexNumber(
-            "",
-            valueA.getValueBeforeDecimal(),
-            valueA.getValueAfterDecimal()
-          );
-          if (result.isNegative) {
-            // Slap a minus sign around the operation
-            firstNumber =
-              `\\smash{\\raisebox{-0.75em}{$-\\Bigg($}}` +
-              firstNumber +
-              `\\smash{\\raisebox{-0.75em}{$\\Bigg)$}}`;
-          }
-          output += firstNumber;
-          output += "\\\\\n";
-          let secondNumber = formatLatexNumber(
-            "-",
-            valueB.getValueBeforeDecimal(),
-            valueB.getValueAfterDecimal()
-          );
-          output += secondNumber;
-        }
-
-        output += `\\\\\n\\hline ${formatLatexNumber(
-          result.isNegative ? "-" : "",
-          result.getValueBeforeDecimal(),
-          result.getValueAfterDecimal()
-        )}`;
-
-        output = `\\begin{alignedat}{3}\n${output}\n\\end{alignedat}`;
-        return output;
-      } else if (props.operator == "multiply") {
-        const result = props.valueA.multiply(props.valueB);
-        let valueA = props.valueA;
-        let valueB = props.valueB;
-        let output = "";
-
-        const lastLineLength = valueA.value.length + valueB.value.length - 1;
-        const resultLength = result.value.length ?? 0;
-
-        const requiredPadding =
-          resultLength > lastLineLength ? resultLength - lastLineLength : 0;
-
-        output += `& ${phantomPadding(
-          requiredPadding
-        )} ${mathPrinting.binaryNumberToLatex(valueA, {
-          zeroWidthDecimal: true,
-          groupZeros: false,
-        })} \\times ${mathPrinting.binaryNumberToLatex(valueB, {
-          zeroWidthDecimal: true,
-          groupZeros: false,
-        })} \\\\\n`;
-
-        output += `\\hline \\\\\n`;
-
-        // Additions
-        const aBits = `\\mathtt{${printBitArray(valueA.value)}}`;
-
-        let extraZeros = 0;
-        for (let i = 0; i < valueB.value.length; i++) {
-          const bitB = valueB.value[i];
-          if (bitB) {
-            output += `&${phantomPadding(
-              i - extraZeros + requiredPadding
-            )} \\mathtt{${"0".repeat(extraZeros)}} ${aBits} \\\\\n`;
-            extraZeros = 0;
-          } else {
-            extraZeros++;
-          }
-        }
-
-        // If the last line is "missing", add it
-        if (extraZeros) {
-          extraZeros--;
-          output += `&${phantomPadding(
-            valueB.value.length - 1 - extraZeros + requiredPadding
-          )} \\mathtt{${"0".repeat(extraZeros + valueA.value.length)}} \\\\\n`;
-        }
-
-        output += `\\hline \\\\\n`;
-
-        // Result
-        output += `&${phantomPadding(
-          lastLineLength - resultLength + requiredPadding
-        )} ${mathPrinting.binaryNumberToLatex(result, {
-          zeroWidthDecimal: true,
-          groupZeros: false,
-        })}`;
-        output = `\\def\\arraystretch{0.1}\n\\begin{alignedat}{1}\n${output}\n\\end{alignedat}`;
-        return output;
-      } else if (props.operator == "divide") {
-        // TODO: Catch division by zero
-        const result = props.valueA.divide(props.valueB, placesAfterDecimal);
-        let valueA = props.valueA;
-        let valueB = props.valueB;
-        let output = "";
-
-        output += `&${phantomPadding(1)} ${mathPrinting.binaryNumberToLatex(
-          valueA,
-          {
-            zeroWidthDecimal: true,
-            groupZeros: false,
-          }
-        )} ${phantomPadding(2)} \\mathllap{\\div} ${phantomPadding(
-          1
-        )} ${mathPrinting.binaryNumberToLatex(valueB, {
-          zeroWidthDecimal: true,
-          groupZeros: false,
-        })} ${phantomPadding(2)} \\mathllap{=} ${phantomPadding(1)}`;
-
-        // Binary division
-        // Shift the decimal points away
-        const maxDecimal = Math.max(valueA.decimalPoint, valueB.decimalPoint);
-        const a = valueA.setSign(false).multiplyByPowerOfTwo(maxDecimal);
-        const b = valueB
-          .setSign(false)
-          .multiplyByPowerOfTwo(maxDecimal)
-          .trimZerosBeforeDecimal();
-
-        if (b.value.length <= 0) {
-          output += `\\text{Division by zero}`;
-        } else {
-          output += ` ${mathPrinting.binaryNumberToLatex(result.result, {
-            zeroWidthDecimal: true,
-            groupZeros: false,
-          })}\\\\\n`;
-
-          // Copy of binary-number.ts
-          let index = 0;
-          const maxLength = a.value.length + placesAfterDecimal; // TODO: Configureable
-          const resultBits = new Array<boolean>(maxLength).fill(false);
-          let remainder = new BinaryNumber(false, [], 0);
-
-          const printPadded = (
-            value: string,
-            valueLength: number,
-            length: number
-          ) =>
-            `&${phantomPadding(
-              Math.max(length - valueLength, 0)
-            )} \\mathtt{${value}} \\\\\n`;
-
-          while (!a.isZero() && index < maxLength) {
-            // Pull down the next bit
-            const newBit = index < a.value.length ? a.value[index] : false;
-            remainder = new BinaryNumber(
-              false,
-              remainder.value.concat(newBit),
-              0
-            );
-
-            const remainderToPrint = remainder
-              .trimZerosBeforeDecimal()
-              .extend(1);
-
-            output += printPadded(
-              printBitArray(remainderToPrint.value),
-              remainderToPrint.value.length,
-              1 + remainder.value.length
-            );
-
-            // If we can subtract, do so
-            if (remainder.compareTo(b) >= 0) {
-              remainder = remainder.subtract(b);
-              resultBits[index] = true;
-
-              const paddingDelta =
-                remainderToPrint.value.length - b.value.length;
-
-              output += printPadded(
-                `\\mathllap{-}\\underline{${phantomPadding(
-                  paddingDelta
-                )}${printBitArray(b.value)}}`,
-                remainderToPrint.value.length,
-                1 + remainder.value.length
-              );
-            } else {
-              resultBits[index] = false;
-              output += printPadded(
-                `\\mathllap{-}\\underline{${phantomPadding(
-                  remainderToPrint.value.length - 1
-                )}0}`,
-                remainderToPrint.value.length,
-                1 + remainder.value.length
-              );
-            }
-            index += 1;
-          }
-
-          let remainderToPrint = new BinaryNumber(
-            false,
-            result.remainder ?? [],
-            0
-          )
-            .trimZerosBeforeDecimal()
-            .extend(1);
-
-          output += printPadded(
-            printBitArray(remainderToPrint.value) + `\\mathrlap{R}`,
-            remainderToPrint.value.length,
-            1 + remainder.value.length
-          );
-        }
-
-        output = `\\def\\arraystretch{0.1}\n\\begin{alignedat}{2}\n${output}\n\\end{alignedat}`;
-        return output;
-      }
-
-      return "";
-    });
     onMounted(() => {
       watch(
         output,
