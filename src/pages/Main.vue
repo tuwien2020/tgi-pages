02c09fbe56afb3de8f79b889d736ee5c83bcceae
Daniel Fenz <leinadznef@gmail.com>
2021-08-27 19:44:12 +0200

    created new main page

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,20 +1,20 @@
 <template>
   <h1>Overview Page</h1>
-    <a-card v-for="category in groupedPages" :key="category.key" v-bind:title=category.key :bordered="true" style="width: 30vw">
+  <section class="columns is-multiline">
+    <a-card v-for="category in pagesByCategory" :key="category.key.number" v-bind:title="category.key.number.toString() + '. ' + category.key.name" :hoverable="true" :bordered="true" class="column is-one-third">
       <div v-for="item in category.values" :key="item.name">
         <router-link v-if="item.internal" :to="item.link">{{item.name}}</router-link>
         <a target="_blank" v-if="!item.internal" :href="item.link">{{item.name}}</a>
       </div>
       
     </a-card>
-  
+  </section>
 </template>
 
 <script lang="ts">
 import { defineComponent, computed, ref, watch, watchEffect } from "vue";
-import pages from "./../assets/navigation";
+import { pagesByCategory, getCategory } from "./../assets/navigation";
 import { Card } from "ant-design-vue";
-import { groupBy } from "./../assets/utilities";
 // import * as cat from "./../assets/grammar-ite";
 // console.log(cat);
 
@@ -24,10 +24,10 @@ export default defineComponent({
   components: {},
   setup() {
     const count = ref(0);
-    const groupedPages = groupBy(pages, page => page.category);
     return {
       count,
-      groupedPages
+      pagesByCategory,
+      getCategory
     };
   },
 });
