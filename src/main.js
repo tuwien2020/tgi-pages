f3221f238a19c96b6837875ab8166c3054ec8fae
Daniel Fenz <leinadznef@gmail.com>
2021-08-27 21:41:13 +0200

    config change, vite import plugin

--- src/main.js
+++ src/main.js
@@ -1,40 +1,28 @@
-import { createApp } from "vue";
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createApp, nextTick, onBeforeMount, onMounted, reactive, ref, watch } from "vue";
+import { createRouter, createWebHashHistory, useRoute, useRouter } from "vue-router";
 import App from "./App.vue";
 import "./index.css";
 import "katex/dist/katex.css";
-import Overview from "./pages/Overview.vue";
-import HammingCode from "./pages/HammingCode.vue";
-import TruthTable from "./pages/TruthTable.vue";
-import HammingDistance from "./pages/HammingDistance.vue";
-import BinaryCalculator from "./pages/BinaryCalculator.vue";
-import BinaryCoding from "./pages/BinaryCoding.vue";
-import Pipeline from "./pages/Pipeline.vue";
-import InterleavingGraph from "./pages/InterleavingGraph.vue";
-import CacheCalc from "./pages/CacheCalc.vue";
-import Stack from "./pages/Stack.vue";
-import Micro16Decompiler from "./pages/Micro16Decompiler.vue";
-import Main from "./pages/Main.vue";
 import { name, version } from "./../package.json";
 import "bulma/css/bulma.css";
 import 'ant-design-vue/dist/antd.css';
-import Antd from "ant-design-vue";
-import { pages } from "./assets/navigation";
-
-/** @type {import("vue-router").RouteRecordRaw[]} */
-const routes = pages.map(p => {return {path: '/' + p.link, component: p.page}});
-routes.push({path: '/', component: Main});
+import router from "./router";
+import Antd from 'ant-design-vue';
 
 if (import.meta.env.PROD) {
   console.log(`${name} - ${version}`);
 }
+const app = createApp(App);
+app.use(router);
+app.use(Antd);
 
-createApp(App)
-  .use(
-    createRouter({
-      history: createWebHashHistory(),
-      routes: routes,
-    })
-  )
-  .use(Antd)
-  .mount("#app");
+app.config.globalProperties.$ref = ref;
+app.config.globalProperties.$reactive = reactive;
+app.config.globalProperties.$onBeforeMount = onBeforeMount;
+app.config.globalProperties.$onMounted = onMounted;
+app.config.globalProperties.$watch = watch;
+app.config.globalProperties.$nextTick = nextTick;
+app.config.globalProperties.$useRouter = useRouter;
+app.config.globalProperties.$useRoute = useRoute;
+
+app.mount("#app");
---
+++ src/router/index.ts
@@ -0,0 +1,11 @@
+import { pages } from "./../assets/navigation";
+import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
+import Main from "./../pages/Main.vue";
+
+const routes = pages.map(p => {return {path: '/' + p.link, component: p.page} as RouteRecordRaw});
+routes.push({path: '/', component: Main});
+
+export default createRouter({
+  history: createWebHashHistory(),
+  routes: routes,
+});
