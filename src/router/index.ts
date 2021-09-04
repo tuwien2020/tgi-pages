ffc1babcf4a28a0dfeee6365ebb76d79b120cb62
Daniel Fenz <dani.fenz@aon.at>
2021-09-04 15:15:42 +0200

    added name attribute to route entries

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,9 +3,9 @@ import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import Main from "./../pages/Main.vue";
 
 const routes = pages.map((p) => {
-  return { path: "/" + p.link, component: p.page } as RouteRecordRaw;
+  return { path: "/" + p.link, component: p.page, name: p.name } as RouteRecordRaw;
 });
-routes.push({ path: "/", component: Main });
+routes.push({ path: "/", component: Main, name: "TGI Pages" });
 
 export default createRouter({
   history: createWebHashHistory(),
