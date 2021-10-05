98592d9c78824feb31f784607ec94784df9b7151
Daniel Fenz <dani.fenz@aon.at>
2021-10-05 19:52:16 +0200

    made searchbar a bit cleaner

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- Or I could just have used a datalist -->
   <span class="search-bar" @keyup="onKeyUp">
-    <input type="text" placeholder="Search" v-model="searchText" class="search-input" />
+    <input type="text" placeholder="SEARCH" v-model="searchText" class="search-input" />
     <div class="search-options">
       <div v-for="option in filteredOptions" :key="option.name" :value="option.name">
         <router-link v-if="option.internal" :to="option.link" class="search-link">{{ option.name }}</router-link>
@@ -79,30 +79,47 @@ export default defineComponent({
 <style scoped>
 .search-bar {
   position: relative;
-  height: 24px;
+  height: max(1.4em,30px);
   margin-left: 4px;
   margin-right: 4px;
+  width: max(3em, 200px);
 }
 .search-input {
   background: white;
-  border: 1px solid black;
+  border: 1px solid rgba(144, 144, 144, 0.739);
+  border-radius: 5px;
+  height: max(1.4em,30px);
+  width: max(3em, 200px);
+  margin: 0px 0px 1px 0px;
+  transition: box-shadow 0.5s ease-out;
+  font-family: BlinkMacSystemFont, -apple-system, "Roboto", "Segoe UI", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", "Helvetica", "Arial", sans-serif;
 }
+
+.search-input:hover {
+  box-shadow: 1px 1px 10px 3px #b8b8b883;
+}
+
 .search-options {
   display: none;
   position: absolute;
   left: 0px;
   right: 0px;
+  width: max(3em, 200px);
   top: calc(100% + 4px);
   border-radius: 4px;
   background: white;
   overflow-x: hidden;
   overflow-y: scroll;
   white-space: nowrap;
-  max-height: 50vh;
+  max-height: 50vh;  
+  font-family: BlinkMacSystemFont, -apple-system, "Roboto", "Segoe UI", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", "Helvetica", "Arial", sans-serif;
 }
+
 .search-options > div {
   padding: 4px 8px;
+  transition: box-shadow 0.5s ease-out;
 }
+
 .search-bar:focus-within > .search-options {
   display: initial;
 }
