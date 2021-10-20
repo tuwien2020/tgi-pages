0ca043eb5739a71d071e9c2a5f3dae53fe8297b4
stefnotch <brandmairstefan@gmail.com>
2021-10-20 09:14:56 +0200

    Replace https-localhost with vite preview
    Upside: fewer dependencies
    Downside: we have to trust vite preview to do the right thing

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import vue from "@vitejs/plugin-vue";
 import copy from "rollup-plugin-copy-assets";
 import vuetify from "@vuetify/vite-plugin";
-import { VitePWA } from 'vite-plugin-pwa';
+import { VitePWA } from "vite-plugin-pwa";
 const { resolve } = require("path");
 
 export default {
@@ -9,56 +9,56 @@ export default {
     exclude: ["monaco-editor"],
   },
   plugins: [
-
     vue(),
     copy({
       assets: ["node_modules/monaco-editor/min/vs"],
     }),
     vuetify({ autoImport: true }),
     VitePWA({
-      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      includeAssets: ["favicon.svg", "favicon.ico", "robots.txt", "apple-touch-icon.png"],
       manifest: {
-        name: 'TGI Pages',
-        short_name: 'tgi-pages',
-        description: 'Helpful tools for the TGI Course at the TU Vienna',
-        theme_color: '#80CBC4',
-        strategies: 'injectManifest',
-        srcDir: 'src',
-        filename: 'sw.ts',
+        name: "TGI Pages",
+        short_name: "tgi-pages",
+        description: "Helpful tools for the TGI Course at the TU Vienna",
+        theme_color: "#80CBC4",
+        strategies: "injectManifest",
+        srcDir: "src",
+        filename: "sw.ts",
         icons: [
           {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
           },
           {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          }
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
         ],
         workbox: {
           cleanupOutdatedCaches: false,
-          sourcemap: true
-        }  
+          sourcemap: true,
+        },
       },
-      registerType: 'autoUpdate',
+      registerType: "autoUpdate",
       workbox: {
         cleanupOutdatedCaches: false,
-        sourcemap: true  
-      }
+        sourcemap: true,
+      },
     }),
   ],
   build: {
+    manifest: true,
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
       },
     },
-    sourcemap: process.env.SOURCE_MAP === 'true',
+    sourcemap: process.env.SOURCE_MAP === "true",
   },
-  
+
   /*build: {
     target: "esnext",
   },*/
