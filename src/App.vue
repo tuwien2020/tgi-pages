02aad2367a3f9a539dfb7fe809afaf0b0e45e3a7
stefnotch <brandmairstefan@gmail.com>
2022-02-17 22:55:00 +0100

    Added a little error notification

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,7 @@
   <div class="content">
     <router-view></router-view>
   </div>
+  <Notifications />
   <PWA />
 </template>
 
@@ -20,14 +21,24 @@ import { useRoute } from "vue-router";
 import { useI18n } from "vue-i18n";
 import { version } from "./../package.json";
 import SearchBar, { SearchOption } from "./components/SearchBar.vue";
+import Notifications from "./components/Notifications.vue";
+import * as Notify from "./notify";
 import { searchablePages } from "./router/navigation";
 import PWA from "./pwa/PWA.vue";
 
 export default defineComponent({
-  components: { SearchBar, PWA },
+  components: { SearchBar, Notifications, PWA },
   setup() {
     const route = useRoute();
     const { t } = useI18n();
+    const notificationMessage = ref("");
+
+    window.addEventListener("error", (ev) => {
+      Notify.error("Unhandled error", ev.error);
+    });
+    window.addEventListener("unhandledrejection", (ev) => {
+      Notify.error("Unhandled error (promise)", ev.reason);
+    });
 
     watch(route, ({ name }) => {
       window.document.title = t((name as string) + "");
@@ -43,6 +54,7 @@ export default defineComponent({
       t,
       version,
       options,
+      notificationMessage,
     };
   },
 });
---
+++ src/notify.ts
@@ -0,0 +1,46 @@
+import { reactive } from "vue";
+
+type Notification = {
+  type: "log" | "warn" | "error";
+  message: string;
+  details: any;
+};
+
+// TODO: Auto-clear
+// TODO: Duplicate message counter
+export const notifications: Notification[] = reactive([]);
+
+function objToString(value: any) {
+  if (typeof value === "string") {
+    return value;
+  } else {
+    try {
+      return JSON.stringify(value);
+    } catch (e) {
+      return value + "";
+    }
+  }
+}
+
+export function notify(type: "log" | "warn" | "error", message: string, details: any) {
+  const description = objToString(details);
+  console[type](message, description);
+
+  notifications.push({
+    type,
+    message,
+    details: description,
+  });
+}
+
+export function log(message: string, details: any) {
+  notify("log", message, details);
+}
+
+export function warn(message: string, details: any) {
+  notify("warn", message, details);
+}
+
+export function error(message: string, details: any) {
+  notify("error", message, details);
+}
