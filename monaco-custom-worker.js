01fd14c346fb050442118cc37cced3817ac37d8f
Yahya Jabary <jabaryyahya@gmail.com>
2023-03-08 15:31:51 +0100

    Updates

---
+++ monaco-custom-worker.js
@@ -0,0 +1,32 @@
+/**
+ *
+ * @param {import('monaco-editor').languages.typescript.TypeScriptWorker} TypeScriptWorker
+ */
+self.customTSWorkerFactory = (TypeScriptWorker) => {
+  // TODO: Line numbers (before every line, insert a "lineNumber = X" statement), used for printing
+  // TODO: Registers like R3 should just work
+  // TODO: Proper debugging (simply interleaving yield linenumber; doesn't really always work)
+  // TODO: Prevent assignment in if
+
+  return class MonacoTSWorker extends TypeScriptWorker {
+    async getVariables(modelUri) {
+      const purr = this._languageService.getProgram(modelUri);
+      const variables = [];
+
+      purr
+        .getSourceFile(modelUri)
+        .identifiers.forEach((key) => variables.push("" + key));
+
+      return variables;
+      /*console.log(this);
+      console.log(ts);
+      console.log(this._ctx.getMirrorModels());
+      console.log(this._languageService.getEmitOutput(modelUri));
+      console.log(this._languageService.getProgram(modelUri));*/
+      /*
+      console.log(purr);
+      console.log(purr.getSourceFile(modelUri)); // .getSourceFiles());
+*/
+    }
+  };
+};
